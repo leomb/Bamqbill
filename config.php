34f67f75bbe3e26f1a2681bd3d14889d6434babd
Leonardo Borsten <leomb@prepressmiami.com>
2024-02-28 16:45:54 -0500

    Add global variables for email address of senders and recipients.

--- config.php
+++ config.php
@@ -1,19 +1,19 @@
 <?php
 session_start();
 
-define('SITE_ROOT','https://www.revu.us/');
-define('SITE_PATH','/home/leomb/revu.us/bamqbill/');
-define('FILE_ROOT','/home/leomb/revu.us/bamqbill/');
+define('SITE_ROOT','localhost:8080/bamqbill/');
+define('SITE_PATH','localhost:8080/bamqbill/');
+define('FILE_ROOT','localhost:8080/bamqbill/');
 
 // DB
 define('DB_SERV', 'localhost');
-define('DB_USER', 'leomb_ams');
-define('DB_PASS', '6J8ymGMiQo}Q');
-define('DB_NAME', 'leomb_ams');
+define('DB_USER', 'root');
+define('DB_PASS', 'mia33187');
+define('DB_NAME', 'ams');
 
 // EMAIL
 define('E_TO', 'lborsten@mac.com');
-define('E_FROM', 'admin@revu.us');
+define('E_FROM', 'leonardoborsten@Leonardos-iMac-2.local');
 define('E_CC', 'leonardoborsten@yahoo.com,leonardoborsten@live.com');
 define('E_BCC', 'leonardo.borsten@aol.com,leonardoborsten@gmail.com');
 define('E_NAME', 'AOG Invoice');
